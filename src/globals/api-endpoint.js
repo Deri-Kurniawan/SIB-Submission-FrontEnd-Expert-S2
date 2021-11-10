97d8cef6f1c43b5f38a131b3f9cdc4c79c49369d
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-10 12:51:40 +0700

    fix endpoint path

--- src/globals/api-endpoint.js
+++ src/globals/api-endpoint.js
@@ -3,10 +3,10 @@ import CONFIG from './config';
 const { BASE_URL, BASE_IMAGE_URL } = CONFIG;
 
 const API_ENDPOINT = {
-  LIST: `${BASE_URL}`,
-  DETAIL: (id) => `${BASE_URL}/${id}`,
-  SEARCH: (keyword) => `${BASE_URL}/search?q=${keyword}`,
-  REVIEW: `${BASE_URL}/review`,
+  LIST: `${BASE_URL}list`,
+  DETAIL: (id) => `${BASE_URL}${id}`,
+  SEARCH: (keyword) => `${BASE_URL}search?q=${keyword}`,
+  REVIEW: `${BASE_URL}review`,
   IMAGE_SMALL: (pictureId) => `${BASE_IMAGE_URL}small/${pictureId}`,
   IMAGE_MEDIUM: (pictureId) => `${BASE_IMAGE_URL}medium/${pictureId}`,
   IMAGE_LARGE: (pictureId) => `${BASE_IMAGE_URL}large/${pictureId}`,
