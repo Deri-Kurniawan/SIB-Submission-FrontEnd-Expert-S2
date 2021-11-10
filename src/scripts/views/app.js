28e15442cca5b01f148658597d86a70a472f8513
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-10 17:18:02 +0700

    adding initial app shell

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,9 +1,26 @@
 import UrlParser from '../routes/url-parser';
 import routes from '../routes/routes';
+import DrawerInitiator from '../utils/drawer-initiator';
 
 class App {
-  constructor({ content }) {
+  constructor({
+    button, icon, drawer, content,
+  }) {
+    this._button = button;
+    this._icon = icon;
+    this._drawer = drawer;
     this._content = content;
+
+    this._initialAppShell();
+  }
+
+  async _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      icon: this._icon,
+      drawer: this._drawer,
+      content: this._content,
+    });
   }
 
   async renderPage() {
