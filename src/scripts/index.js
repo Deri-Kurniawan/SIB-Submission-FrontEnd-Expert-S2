69ae89b16a7267aaa4e86539d551ead91deb35b2
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-10 17:19:06 +0700

    remove main & adding property for app constructor

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,13 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '@fortawesome/fontawesome-free/js/all';
 import '../styles/main.css';
 import '../styles/responsive.css';
-import main from './views/main';
+// import main from './views/main';
 import App from './views/app';
 
 const app = new App({
+  button: document.querySelector('#menu-toggle'),
+  icon: document.querySelector('#menu-toggle .icon'),
+  drawer: document.querySelector('.nav-menu'),
   content: document.querySelector('#mainContent'),
 });
 
@@ -16,5 +19,3 @@ window.addEventListener('haschange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
 });
-
-document.addEventListener('DOMContentLoaded', main);
