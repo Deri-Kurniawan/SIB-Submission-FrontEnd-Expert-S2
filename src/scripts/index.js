f51bdf4a45d547b3479faa32f66961e094b30f7b
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-10 16:08:43 +0700

    initialize App c;lass object & rendering page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,19 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '@fortawesome/fontawesome-free/js/all';
 import '../styles/main.css';
 import '../styles/responsive.css';
-import main from './view/main';
+import main from './views/main';
+import App from './views/app';
+
+const app = new App({
+  content: document.querySelector('#mainContent'),
+});
+
+window.addEventListener('haschange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
 
 document.addEventListener('DOMContentLoaded', main);
