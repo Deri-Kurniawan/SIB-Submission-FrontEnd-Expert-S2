549cce9f2d46f4aed1f913facb9c386f14867440
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-12 00:46:56 +0700

    adding accesibility

--- src/scripts/utils/detail-helper.js
+++ src/scripts/utils/detail-helper.js
@@ -4,7 +4,7 @@ const DetailHelper = {
     foods.forEach((food) => {
       foodsString += `
       <span class="food-item">
-        <a href="https://google.com/search?q=Makanan ${food.name} adalah" target="_blank" rel=”noreferrer” title="Apa itu Makanan ${food.name}?">${food.name}</a>
+        <a href="https://google.com/search?q=Makanan ${food.name} adalah" target="_blank" rel=”noreferrer” aria-label="${food.name}." title="Apa itu Makanan ${food.name}?">${food.name}</a>
       </span>`;
     });
     return foodsString;
@@ -15,7 +15,7 @@ const DetailHelper = {
     drinks.forEach((drink) => {
       drinksString += `
       <span class="drink-item">
-        <a href="https://google.com/search?q=Minuman ${drink.name} adalah" target="_blank" rel=”noreferrer” title="Apa itu Minuman ${drink.name}?">${drink.name}</a>
+        <a href="https://google.com/search?q=Minuman ${drink.name} adalah" target="_blank" rel=”noreferrer” aria-label="${drink.name}." title="Apa itu Minuman ${drink.name}?">${drink.name}</a>
       </span>`;
     });
     return drinksString;
@@ -25,8 +25,8 @@ const DetailHelper = {
     customerReviews.forEach((customerReview) => {
       customerReviewsString += `
         <div class="customer-review-item">
-          <span>${customerReview.name}</span> - ${customerReview.date}
-          <p>${customerReview.review}</p>
+          <span tabindex="0" aria-label="${customerReview.name} memberikan review pada ${customerReview.date}.">${customerReview.name} - ${customerReview.date}</span>
+          <p tabindex="0" aria-label="pesan reviewnya adalah ${customerReview.review}.">${customerReview.review}</p>
         </div>`;
     });
     return customerReviewsString;
