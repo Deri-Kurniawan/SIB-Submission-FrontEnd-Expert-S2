9ee03922f31cc1c92ef60ed0efa37506bd2b8ead
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-12 13:37:45 +0700

    adding restaurant review initiator

---
+++ src/scripts/utils/restaurant-review-initiator.js
@@ -0,0 +1,47 @@
+import API_ENDPOINT from '../globals/api-endpoint';
+import UrlParser from '../routes/url-parser';
+import { createRestaurantFormReviewTemplate } from '../views/templates/template-creator';
+
+const RestaurantReviewInitiator = {
+  init({ restaurantReviewContainer }) {
+    this._restaurantReviewContainer = restaurantReviewContainer;
+    this._renderForm();
+  },
+
+  _renderForm() {
+    this._restaurantReviewContainer.innerHTML = createRestaurantFormReviewTemplate();
+    const reviewFormElement = document.querySelector('#reviewForm');
+    reviewFormElement.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this._isFormSubmitted(e);
+    });
+  },
+
+  _isFormSubmitted() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const inputName = document.querySelector('#inputName');
+    const inputReview = document.querySelector('#inputReview');
+
+    this._makeRequest({ id: url.id, name: inputName.value, review: inputReview.value });
+  },
+
+  async _makeRequest({ id, name, review }) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id, name, review,
+      }),
+    };
+
+    const responseText = await fetch(API_ENDPOINT.ADD_REVIEW, options);
+    const responseJson = await responseText.json();
+    if (!responseJson.error) {
+      alert('Review has been added!');
+    }
+  },
+};
+
+export default RestaurantReviewInitiator;
