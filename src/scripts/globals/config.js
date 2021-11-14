907ba9b5c8f21863a6fd4dafa28c006b20230a8d
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-14 17:33:18 +0700

    change cache to static str

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/',
   DEFAULT_LANGUAGE: 'en-es',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'DZRESTO_CACHE',
   DATABASE_NAME: 'restaurant-exploler-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurant',
