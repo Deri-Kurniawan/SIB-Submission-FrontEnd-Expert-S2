72a614cdfcfd3610f96f2cd27e263d360dadad92
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-14 17:26:32 +0700

    adding multiplatform manifest icons

--- webpack.common.js
+++ webpack.common.js
@@ -65,6 +65,23 @@ module.exports = {
           size: '1024x1024',
           purpose: 'maskable',
         },
+        {
+          src: path.resolve('src/public/images/favicon/brand-logo.png'),
+          sizes: [120, 152, 167, 180, 1024],
+          destination: path.join('icons', 'ios'),
+          ios: true,
+        },
+        {
+          src: path.resolve('src/public/images/favicon/brand-logo.png'),
+          size: 1024,
+          destination: path.join('icons', 'ios'),
+          ios: 'startup',
+        },
+        {
+          src: path.resolve('src/public/images/favicon/brand-logo.png'),
+          sizes: [36, 48, 72, 96, 144, 192, 512],
+          destination: path.join('icons', 'android'),
+        },
       ],
     }),
   ],
