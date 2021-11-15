e4afb9da298f6375bf12562b2e3daa5e536827ae
Deri Kurniawan <deri.netuchi@gmail.com>
2021-11-15 11:47:26 +0700

    fix typo language config

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/',
-  DEFAULT_LANGUAGE: 'en-es',
+  DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: 'DZRESTO_CACHE',
   DATABASE_NAME: 'restaurant-exploler-database',
   DATABASE_VERSION: 1,
